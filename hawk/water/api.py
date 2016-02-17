8da48edd8b2c6fe6d75cb9e97bc0df5991d8cab5
DyvannoPC <chiefvand@gmail.com>
2016-02-17 17:32:07 +0700

    api, live-pretreatment, live-product, live-reject, urls

--- hawk/water/api.py
+++ hawk/water/api.py
@@ -13,3 +13,19 @@ db = client.inetscada
 def live_pretreatment_api(request):
     message =  dumps(list(db.glm.find().sort("_id",-1).limit(1)))
     return HttpResponse(message, content_type='application/json') 
+
+def live_osmosis_api(request):
+    message =  dumps(list(db.glm.find().sort("_id",-1).limit(1)))
+    return HttpResponse(message, content_type='application/json') 
+
+def live_product_api(request):
+    message =  dumps(list(db.glm.find().sort("_id",-1).limit(1)))
+    return HttpResponse(message, content_type='application/json') 
+
+def live_reject_api(request):
+    message =  dumps(list(db.glm.find().sort("_id",-1).limit(1)))
+    return HttpResponse(message, content_type='application/json') 
+
+def live_energy_api(request):
+    message =  dumps(list(db.glm.find().sort("_id",-1).limit(1)))
+    return HttpResponse(message, content_type='application/json') 
