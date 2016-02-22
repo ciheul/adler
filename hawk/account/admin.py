fed45536829b9608bea738d0eabd2b87fba43f0c
Winnu Ayi Satria <winnuayi@gmail.com>
2016-02-22 18:27:27 +0700

    add permissionsmixin and update model

--- hawk/account/admin.py
+++ hawk/account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import HawkUser
+
+
+admin.site.register(HawkUser)
--- hawk/account/models.py
+++ hawk/account/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -15,19 +15,26 @@ class Asset(models.Model):
     lat = models.FloatField()
 
 
-class HawkUser(AbstractBaseUser):
+class HawkUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(_('username'), max_length=30, unique=True)
     email = models.EmailField(_('email address'), max_length=254, unique=True)
-    full_name = models.CharField(_('full name'), max_length=100)
+    full_name = models.CharField(_('full name'), max_length=100,
+                                 null=True, blank=True)
     phone = models.CharField(_('phone number'), max_length=40, unique=True)
     organization = models.ForeignKey(Organization, null=True, blank=True)
-    picture = models.ImageField()
+    picture = models.ImageField(null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    # is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
     objects = UserManager()
+
+    # def get_full_name(self):
+    #     return self.full_name
+
+    def get_short_name(self):
+        return self.full_name
