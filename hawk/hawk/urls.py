09cdde1961f8bd9bc0ef9878902c7621b41e35b4
DyvannoPC <chiefvand@gmail.com>
2016-02-24 11:34:43 +0700

    urls

--- hawk/hawk/urls.py
+++ hawk/hawk/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('account.urls')),
     url(r'^water/', include('water.urls')),
-    #url(r'^', views.index),
     url(r'^', include('account.urls'))
 ]
