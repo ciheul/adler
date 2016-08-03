dccd53276b343210b126f8c4f928cb3c1bac7430
Winnu Ayi Satria <winnuayi@gmail.com>
2016-08-03 15:11:15 +0700

    hotfix: cctv, getRandomInt

--- adler/crontab_sc.py
+++ adler/crontab_sc.py
@@ -9,5 +9,5 @@ import os
 #subprocess.call(["wget","-P","/home/ciheul/Projects/inetscada/app/adler/hawk/water/images","http://10.212.0.10/CCTV/GLM/SWRO_001/CCTV1.jpg","-O","cctv.jpg"])
 
 # PRODUCTION
-os.chdir("/home/smartadmin/inetscada/app/hawk/static/water/images")                
-subprocess.call(["wget","-P","/home/smartadmin/inetscada/app/hawk/static/water/images","http://10.212.0.10/CCTV/GLM/SWRO_001/CCTV1.jpg","-O","cctv.jpg"])
+os.chdir("/home/smartadmin/inetscada/app/glm/static/water/images")                
+subprocess.call(["wget","-P","/home/smartadmin/inetscada/app/glm/static/water/images","http://10.212.0.10/CCTV/GLM/SWRO_001/CCTV1.jpg","-O","cctv.jpg"])
--- glm/static/water/backboneapp/scripts/misc/utility.js
+++ glm/static/water/backboneapp/scripts/misc/utility.js
@@ -24,4 +24,6 @@ function validateEmailRegex(email) {
 }
 
 
-// export { getCookie, validateEmailRegex }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
